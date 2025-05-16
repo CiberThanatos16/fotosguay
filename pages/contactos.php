98754ddd0115c8672b32ec9c2fddc184208c32af
CiberThanatos16 <51860591+CiberThanatos16@users.noreply.github.com>
2025-05-15 19:12:19 -0600

    Update contactos.php

--- pages/contactos.php
+++ pages/contactos.php
@@ -1,37 +1,38 @@
-<?php
-include_once "Plantillas/head.php";
-include_once "Plantillas/BarraNav.php";
-?>
-
 <section class="container">
     <div class="p-5 text text-center">
         <h1>Mis redes</h1>
     </div>
-    <div class="m-5">
-        <div class="card border-primary mb-3" style="width: 18rem; height: 22rem;">
-            <a href="">
-                <img src="images/imgF.png" class="card-img-top" style="width: 200px; height: 200px;" alt="...">
-                <div class="card-body text-center">
-                    <h5 class="card-title">FACEBOOK</h5>
+
+    <div class="card-group">
+        <div class="card border-warning mb-3">
+            <a href="" class="text-warning" target="_blank">
+                <div class="text-center icon-size200 p-5">
+                    <i class="bi bi-whatsapp"></i>
+                </div>
+                <div class="card-body text-center text-warning">
+                    <h2>whatssapp</h2>
                 </div>
             </a>
         </div>
-        <div class="card border-primary mb-3" style="width: 18rem; height: 22rem; ">
+        <div class="card border-primary mb-3">
             <a href="">
-                <img src="images/R.png" class="card-img-top" style="width: 200px; height: 200px;" alt="...">
+                <div class="text-center icon-size200 p-5">
+                    <i class="bi bi-facebook"></i>
+                </div>
                 <div class="card-body text-center">
-                    <h5 class="card-title">WHATSAPP</h5>
+                    <h2>facebook</h2>
                 </div>
             </a>
         </div>
-        <div class="card border-primary mb-3" style="width: 18rem; height: 22rem;">
+        <div class="card border-tertiary mb-3">
             <a href="">
-                <img src="images/imgI.jpeg" class="card-img-top" style="width: 200px; height: 200px;" alt="...">
-                <div class="card-body text-center">
-                    <h5 class="card-title">INSTAGRAM</h5>
+                <div class="text-center icon-size200 p-5">
+                    <i class="bi bi-instagram"></i>
+                </div>
+                <div class="card-body text-center text-tertiary">
+                    <h2>instagram</h2>
                 </div>
             </a>
         </div>
-        
     </div>
 </section>
